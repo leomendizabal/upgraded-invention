60b80b71b2c6936417a5e5682f464ae04d943c7e
Rodrigo <rgordano@hotmail.com>
2018-02-10 17:24:44 -0300

    eclipse commit

--- src/bedelia/Main.java
+++ src/bedelia/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("andando...!");
 		System.out.println("anda anda");
-		System.out.println("rompe pepe");
+		System.out.println("rompe pepe hola");
 		
 	}
 
