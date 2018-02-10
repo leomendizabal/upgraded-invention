dc9c8522dbf87da61b3491c61863dc1ed0607778
Leo <l.mendizabal.cds@gmail.com>
2018-02-10 19:15:46 -0300

    becado e inscripcion

---
+++ src/edu/ude/bedelia/logica/entidades/Inscripcion.java
@@ -0,0 +1,61 @@
+package edu.ude.bedelia.logica.entidades;
+
+public class Inscripcion {
+	private Integer numero;
+	private int anio;
+	private float montoBase;
+	private int calificacion;
+	private Asignatura asignatura;
+	
+	public Inscripcion(Integer numero, int anio, float montoBase, int calificacion, Asignatura asignatura) {
+		this.numero = numero;
+		this.anio = anio;
+		this.montoBase = montoBase;
+		this.calificacion = calificacion;
+		this.asignatura = asignatura;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	public float getMontoBase() {
+		return montoBase;
+	}
+
+	public void setMontoBase(float montoBase) {
+		this.montoBase = montoBase;
+	}
+
+	public int getCalificacion() {
+		return calificacion;
+	}
+
+	public void setCalificacion(int calificacion) {
+		this.calificacion = calificacion;
+	}
+
+	public Asignatura getAsignatura() {
+		return asignatura;
+	}
+
+	public void setAsignatura(Asignatura asignatura) {
+		this.asignatura = asignatura;
+	}
+	
+	boolean esAprobada() {
+		return this.calificacion >= 6;
+	}
+}
