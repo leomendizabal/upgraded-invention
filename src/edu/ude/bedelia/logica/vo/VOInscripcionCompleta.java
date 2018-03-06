b1f1a9e3fe23902080704b963ee5a3b83edc3236
Santiago <santimattius@gmail.com>
2018-03-06 02:03:24 -0300

    Commit inscipciones

--- src/edu/ude/bedelia/logica/vo/VOInscripcionCompleta.java
+++ src/edu/ude/bedelia/logica/vo/VOInscripcionCompleta.java
@@ -1,11 +1,26 @@
 package edu.ude.bedelia.logica.vo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.ude.bedelia.logica.entidades.Inscripcion;
 
 public class VOInscripcionCompleta extends VOInscripcion {
 
 	private float montoBase;
+	public final static List<String> attrsCompleto = new ArrayList<String>() {
 
+		private static final long serialVersionUID = 1L;
+
+		{
+			add("número de inscripción");
+			add("asignatura");
+			add("año lectivo");
+			add("calificación");
+			add("monto base");
+		}
+	};
+	
 	public VOInscripcionCompleta(Integer numero, int anio, float montoBase, int calificacion) {
 		super(numero, anio, montoBase, calificacion);
 		this.montoBase = montoBase;
@@ -19,4 +34,7 @@ public class VOInscripcionCompleta extends VOInscripcion {
 	public float getMontoBase() {
 		return montoBase;
 	}
+
+	
+	
 }
